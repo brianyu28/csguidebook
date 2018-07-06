8467bc8a22231f91a3a2e30efbac02cbe4f3f696
Brian Yu <brianyu28@gmail.com>
2018-07-05 21:24:23 -0400

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
+import * as ReactGA from "react-ga";
 
 import Guide from "./components/Guide";
 import Welcome from "./components/Welcome";
 
 import "./css/App.css";
 
+ReactGA.initialize("UA-121924288-1");
+
+const Analytics = () => {
+    ReactGA.pageview(window.location.pathname);
+    return null;
+}
+
 class App extends React.Component {
 
   public render() {
     return (
         <div className="app">
+            <Route pattern="/" component={Analytics} />
             <Route exact path="/" component={Welcome} />
             <Route path="/guide/:guideSlug" component={Guide} />
         </div>
