b57a65743877a090a39763fa5ad7fb90584d473b
Brian Yu <brianyu28@gmail.com>
2018-07-06 21:12:07 -0400

    Factor out link generation

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,20 +5,31 @@ import Header from "./Header";
 
 export default class Welcome extends React.Component {
 
-  public render() {
-    return (
-        <div className="homepage-header">
-            <Header />
-            <img id="homepage-logo" src="/assets/img/logo.png" />
-            <h1>CS Guidebook</h1>
-            Learn about...
-            <br/><br/>
-            <Link to="/guide/git">Git</Link>
-            <br/><br/>
-            <hr/>
-            <br/>
-            CS Guidebook is a work in progress by <a href="https://www.brianyu.me" target="_blank">Brian Yu</a>.
-        </div>
-    );
-  }
+    private linkData = [
+        {"name": "Git", "url": "git"},
+        {"name": "Graph Theory", "url":"graph-theory"}
+    ];
+
+    public render() {
+
+        const links = this.linkData.map((link) => (
+            <div>
+                <Link to={"/guide/" + link.url}>{link.name}</Link>
+                <br/>
+            </div>
+        ));
+
+        return (
+            <div className="homepage-header">
+                <Header />
+                <img id="homepage-logo" src="/assets/img/logo.png" />
+                <h1>CS Guidebook</h1>
+                Learn about...
+                <br/><br/>
+                {links}
+                <br/><hr/><br/>
+                CS Guidebook is a work in progress by <a href="https://www.brianyu.me" target="_blank">Brian Yu</a>.
+            </div>
+        );
+    }
 }
